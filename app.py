3bf0cc96340dbbcb4ab948f0f5c03f754cac5a76
Sleyva29 <sleyva29@hotmail.com>
2024-03-04 08:43:58 -0600

    Add Flask

---
+++ app.py
@@ -0,0 +1,23 @@
+from flask import Flask, request
+from views import main
+
+app = Flask(__name__)
+
+
+@app.route('/ping')
+def ping():
+    response = {'status': 'success', 'message': 'pong'}
+    return response, 200
+
+@app.route('/last_played')
+def etl_sp():
+    date = request.args.get('date')
+    user_name = request.args.get('user')
+    data = main.main(date,user_name)
+    response = {'status': 'success', 'data': data}
+    return response, 200
+    
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
--- views/main.py
+++ views/main.py
@@ -1,11 +1,14 @@
-import extract
-import transform
-import load
+import sys
+sys.path.append('..')
+from views import extract
+from views import transform
+from views import load
 
 def main(date,username):
     ext = extract.recently_played(date,5)
     trans = transform.filt_art_and_song(ext)
     load.insert_played_songs(username,trans,date)
+    return trans
     
 if __name__ == '__main__':
     main('2024-03-02','Enrique')
